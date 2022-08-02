3f9222fa6234d65dcfb07502a463ac6b7a8d98d6
phasetr <phasetr@gmail.com>
2022-08-02 16:24:49 +0900

    Update

--- libs/data/src/data/phys-exprs.ts
+++ libs/data/src/data/phys-exprs.ts
@@ -8,4 +8,6 @@ export const physExprData = {"data":[{"title":"ルベーグ積分","expression":
 {"title":"シュワルツシルト計量","expression":"\\begin{aligned}c^2 d\\tau^2 = \\rbk{1 - \\frac{2GM}{c^2 r}} c^2 dt^2 - \\frac{1}{1 - \\frac{2GM}{c^2r}} dr^2 - r^2 (d \\theta^2 + \\sin^2 \\theta d \\phi^2).\\end{aligned}","commentary":"球対称で静的な質量分布の外部にある重力場を記述するアインシュタイン方程式の解. ブラックホールを記述できる."},
 {"title":"運動方程式","expression":"\\begin{aligned}m \\frac{d^2 \\vectorr}{dt^2} = \\vectorF.\\end{aligned}","commentary":"高校以来のニュートンの運動方程式."},
 {"title":"ハミルトンの最小作用の原理","expression":"\\begin{aligned}\\delta \\int_{t_i}^{t_f} L \\rbk{q(t), \\dot{q}(t), t} \\dt = 0.\\end{aligned}","commentary":"変分法の原点の一つ."},
-{"title":"オイラー・ラグランジュ方程式","expression":"\\begin{aligned}\\frac{d}{dt} \\frac{\\partial L}{\\partial \\dot{q}_k} - \\frac{\\partial L}{\\partial q_k} = 0.\\end{aligned}","commentary":"最小作用の原理にしたがう経路がみたす微分方程式."}]}
+{"title":"オイラー・ラグランジュ方程式","expression":"\\begin{aligned}\\frac{d}{dt} \\frac{\\partial L}{\\partial \\dot{q}^k} - \\frac{\\partial L}{\\partial q^k} = 0.\\end{aligned}","commentary":"最小作用の原理にしたがう経路がみたす微分方程式."},
+{"title":"ハミルトンの正準方程式","expression":"\\begin{align}\\frac{dq^k}{dt}&=\\frac{\\partial H(q,p,t)}{\\partial p_k}, \\\\ \\frac{dp_k}{dt}&=-\\frac{\\partial H(q,p,t)}{\\partial q^k}.\\end{align}","commentary":"二階の運動方程式が一階の微分方程式系に書き換わっている. 詳しくは解析力学で."},
+{"title":"ハミルトン-ヤコビ方程式","expression":"\\begin{align}\\frac{\\partial S}{\\partial t} + H \\rbk{q, \\frac{\\partial S}{\\partial q},t} = 0.\\end{align}","commentary":"ニュートンの運動方程式やオイラー・ラグランジュ方程式と等価な, 力学を支配する方程式."}]}
