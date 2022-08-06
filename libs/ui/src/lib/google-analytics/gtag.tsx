321be457d458ecbfee05c67e0fe976828c45d2c6
phasetr <phasetr@gmail.com>
2022-08-06 11:04:18 +0900

    Update

--- libs/ui/src/lib/google-analytics/gtag.tsx
+++ libs/ui/src/lib/google-analytics/gtag.tsx
@@ -13,10 +13,6 @@ export const pageView = (path: string) => {
 
 // GAイベントを発火させる
 export const event = ({action, category, label, value = ''}: Event) => {
-  if (!existsGaId) {
-    return
-  }
-
   window.gtag('event', action, {
     event_category: category,
     event_label: label ? JSON.stringify(label) : '',
